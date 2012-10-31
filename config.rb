28bdb298c3efc2c8390d9f8a52fe15fd3a569b45
Max Veytsman <maxim@ontoillogical.com>
2012-10-31 17:02:47 -0400

    Breaking in and out of Vagrant

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/finite/"
+http_images_path = "/finite/images"
+http_fonts_path = "/finite/fonts"
+css_dir = "public/finite/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
